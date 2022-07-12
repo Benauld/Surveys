c2c4ff50296f61262df10e2815a51c0522ad5fd4
benauld <ben@portobello-consultancy.co.uk>
2022-07-12 13:24:03 +0100

    finished auth flow

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const { mongoURI, cookieKey } = require("./config/keys");
+// model defined here - must come above require passport where a new user is set.
+require("./models/User");
+require("./services/passport");
+
+mongoose.connect(mongoURI);
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ hi: "there it's, the second deploy" });
-});
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [cookieKey],
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
