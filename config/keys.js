6fe013d7bcf35f9c265bff167ec9ec1beeaa9fc0
benauld <ben@portobello-consultancy.co.uk>
2022-07-21 12:52:11 +0100

    add console log

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,9 @@
 // figure out what credentials to return
 
 if (process.env.NODE_ENV === "production") {
+  console.log("prod", process.env.NODE_ENV);
   module.exports = require("./prod");
 } else {
+  console.log("dev", process.env.NODE_ENV);
   module.exports = require("./dev");
 }
