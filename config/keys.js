be3f5658450940d5fe6cd349a6ff33ec70b89a0f
benauld <ben@portobello-consultancy.co.uk>
2022-07-21 12:59:40 +0100

    tweak to keys

--- config/keys.js
+++ config/keys.js
@@ -1,9 +1,7 @@
 // figure out what credentials to return
 
 if (process.env.NODE_ENV === "production") {
-  console.log("prod", process.env.NODE_ENV);
   module.exports = require("./prod");
 } else {
-  console.log("dev", process.env.NODE_ENV);
-  module.exports = require("./dev");
+  module.exports = require("./prod");
 }
