405869c2073bd59e126387ce336759ed7f10e67e
benauld <ben@portobello-consultancy.co.uk>
2022-09-13 10:41:02 +0100

    login page

--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -11,31 +11,74 @@ const Login = () => {
       }}
     >
       <div className="container">
+        <div className="section center">
+          <img
+            className="responsive-img center"
+            style={{ width: "250px" }}
+            src={`${window.location.origin}/images/Original_long.png`}
+            alt="X-altd"
+          />
+        </div>
+
         <div style={{ textAlign: "center", flex: "1 0 auto" }}>
-          <h5 class="indigo-text">Einloggen</h5>
-          <div className="row">
-            <div className="input-field col s12">
-              <input id="email" type="email" className="validate" />
-              <label for="email">Email</label>
+          <h5 className="indigo-text">Einloggen</h5>
+          <div className="section"></div>
+          <div className="container">
+            <div
+              className="z-depth-1 grey lighten-4 row"
+              style={{
+                display: "inline-block",
+                padding: "60px 80px 20px 80px",
+                border: "1px solid #EEE",
+              }}
+            >
+              <form className="col s12">
+                <div className="row">
+                  <div className="col s12"></div>
+                </div>
+                <div className="row">
+                  <div className="input-field col s12">
+                    <input id="email" type="email" className="validate" />
+                    <label htmlFor="email">Email</label>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="input-field col s12">
+                    <input id="password" type="password" className="validate" />
+                    <label htmlFor="passwort">Passwort</label>
+                  </div>
+                </div>
+                <div className="row">
+                  <label style={{ float: "right" }}>
+                    <a className="pink-text" href="#!">
+                      <b>Passwort vergessen?</b>
+                    </a>
+                  </label>
+                </div>
+                <div className="row">
+                  <button
+                    className="waves-effect waves-light btn green darken-4"
+                    style={{ width: "196px" }}
+                  >
+                    Login
+                  </button>
+                </div>
+              </form>
+              <div className="row">
+                <a href="/auth/google">
+                  <button style={{ border: "none" }}>
+                    <img
+                      className="responsive-img center"
+                      style={{ width: "200px" }}
+                      src={`${window.location.origin}/btn_google.png`}
+                      alt="Google login"
+                    />
+                  </button>
+                </a>
+              </div>
             </div>
-          </div>
-          <div className="row">
-            <div className="input-field col s12">
-              <input id="password" type="password" className="validate" />
-              <label for="password">Password</label>
-            </div>
-          </div>
-          <div>
             <div className="row">
-              <button className="waves-effect waves-light btn green">
-                Login
-              </button>
-              <div className="row"></div>
-              <a href="/auth/google">
-                <button className="waves-effect waves-light btn blue">
-                  Login with Google
-                </button>
-              </a>
+              <a href="#!">Account registrieren</a>
             </div>
           </div>
         </div>
@@ -45,78 +88,3 @@ const Login = () => {
 };
 
 export default Login;
-
-<body>
-  <div class="section"></div>
-  <main>
-    <center>
-      <div class="section"></div>
-
-      <h5 class="indigo-text">Please, login into your account</h5>
-      <div class="section"></div>
-
-      <div class="container">
-        <div
-          class="z-depth-1 grey lighten-4 row"
-          style="display: inline-block; padding: 32px 48px 0px 48px; border: 1px solid #EEE;"
-        >
-          <form class="col s12" method="post">
-            <div class="row">
-              <div class="col s12"></div>
-            </div>
-
-            <div class="row">
-              <div class="input-field col s12">
-                <input class="validate" type="email" name="email" id="email" />
-                <label for="email">Enter your email</label>
-              </div>
-            </div>
-
-            <div class="row">
-              <div class="input-field col s12">
-                <input
-                  class="validate"
-                  type="password"
-                  name="password"
-                  id="password"
-                />
-                <label for="password">Enter your password</label>
-              </div>
-              <label style="float: right;">
-                <a class="pink-text" href="#!">
-                  <b>Forgot Password?</b>
-                </a>
-              </label>
-            </div>
-
-            <br />
-            <center>
-              <div class="row">
-                <button
-                  type="submit"
-                  name="btn_login"
-                  class="col s12 btn btn-large waves-effect indigo"
-                >
-                  Login
-                </button>
-              </div>
-            </center>
-          </form>
-        </div>
-      </div>
-      <a href="#!">Create account</a>
-    </center>
-
-    <div class="section"></div>
-    <div class="section"></div>
-  </main>
-  <script
-    type="text/javascript"
-    src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.1/jquery.min.js"
-  ></script>
-  <script
-    type="text/javascript"
-    src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min.js"
-  ></script>
-  //
-</body>;
